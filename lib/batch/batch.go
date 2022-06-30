9727c6f6b751769c0aab9ed5d9b60e5cc0253991
Dzianis Lapanik <dzianis_lapanik@epam.com>
2022-06-30 19:30:39 +0300

    attempt #1

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,7 +1,10 @@
 package batch
 
 import (
+	"sync"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type user struct {
@@ -14,5 +17,29 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, 0, n)
+	var i int64
+	var mut sync.Mutex
+	var group errgroup.Group
+
+	group.SetLimit(int(pool))
+
+	for ; i < n; i++ {
+
+		userId := i
+
+		group.Go(func() error {
+			user := getOne(userId)
+
+			mut.Lock()
+			res = append(res, user)
+			mut.Unlock()
+
+			return nil
+		})
+	}
+
+	group.Wait()
+
+	return res
 }
